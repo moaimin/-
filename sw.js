5f71b5c4c9fd4074622c862765b279989355916e
moaimin <wpwpkame@gmail.com>
2026-02-21 21:25:58 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "muscle-tracker-v1";
+const CACHE_NAME = "muscle-tracker-v2";
 const urlsToCache = [
   "./",
   "./index.html",
@@ -18,7 +18,8 @@ self.addEventListener("install", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => caches.match("./index.html"));
+    })
   );
 });
